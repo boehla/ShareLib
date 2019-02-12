5eaa11f8c79f99e14b9f5d9985451b0660f92832
Daniel Böhler <daniel.boehler@hotmail.com>
2019-02-12 20:18:04 +0100

    Added new feature: ValueBuffer

---
+++ UnitTestProject/ValueBufferTest.cs
@@ -0,0 +1,62 @@
+﻿using Lib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject {
+    [TestClass]
+    public class ValueBufferTest {
+        [TestMethod]
+        public void TestValueBuffer() {
+            ValueBuffer vb = new ValueBuffer();
+
+            Dictionary<string, object> testValues = new Dictionary<string, object>();
+            testValues.Add("strVal", "hoi²³²³1234");
+            testValues.Add("intVal", 112312341);
+            testValues.Add("doubleVal", double.MaxValue);
+            testValues.Add("class", new StringBuilder().Append("asdfa"));
+
+            foreach (KeyValuePair<string, object> item in testValues) {
+                vb.setVal(item.Key, item.Value, TimeSpan.FromSeconds(1));
+            }
+            Assert.AreEqual(testValues.Count, vb.TotalCount);
+
+            foreach (KeyValuePair<string, object> item in testValues) {
+                Assert.IsFalse(vb.needsRefresh(item.Key));
+                Assert.AreEqual(item.Value, vb.getVal(item.Key));
+            }
+            DateTime sta = DateTime.UtcNow;
+            while(sta.AddSeconds(1) > DateTime.UtcNow) System.Threading.Thread.Sleep(100);
+
+            // Values are still in there, but expired!
+            foreach (KeyValuePair<string, object> item in testValues) {
+                Assert.IsTrue(vb.needsRefresh(item.Key));
+                Assert.AreEqual(item.Value, vb.getVal(item.Key));
+            }
+            string seckey = "ThisIsANewKey_SoThisNeedsAlawysARefresh";
+            Assert.IsTrue(vb.needsRefresh(seckey));
+
+            vb.setVal(seckey, "value", TimeSpan.FromSeconds(1));
+            
+            vb.cleanOlds();
+
+            Assert.AreEqual("value", vb.getVal(seckey));
+            Assert.AreEqual(1, vb.TotalCount);
+
+            foreach (KeyValuePair<string, object> item in testValues) {
+                Assert.IsTrue(vb.needsRefresh(item.Key));
+                Assert.AreEqual(null, vb.getVal(item.Key));
+            }
+
+            sta = DateTime.UtcNow;
+            while (sta.AddSeconds(1) > DateTime.UtcNow) System.Threading.Thread.Sleep(100);
+
+            vb.cleanOlds();
+
+            Assert.AreEqual(0, vb.TotalCount);
+        }
+    }
+}
---
+++ ValueBuffer.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib {
+    public class ValueBuffer {
+        /// <summary>
+        /// Time for autocleaning the whole buffer; Default 1d;
+        /// </summary>
+        public TimeSpan AutoCleanTimeSpan { get; set; }
+        /// <summary>
+        /// Autoclean is enabled by default
+        /// </summary>
+        public bool AutoCleanEnabled { get; set; }
+        /// <summary>
+        /// Includes also the expired
+        /// </summary>
+        public int TotalCount {
+            get { return vals.Count; }
+        }
+
+        private Dictionary<string, BuffEntry> vals = new Dictionary<string, BuffEntry>();
+        private DateTime lastClean = DateTime.UtcNow;
+        
+        public ValueBuffer() {
+            AutoCleanTimeSpan = TimeSpan.FromDays(1);
+            AutoCleanEnabled = true;
+        }
+        public bool needsRefresh(string key) {
+            if (!vals.ContainsKey(key) || vals[key].Expired) {
+                return true;
+            }
+            return false;
+        }
+        public void setVal(string key, object val, DateTime expiredate) {
+            if (AutoCleanEnabled && lastClean.Add(AutoCleanTimeSpan) < DateTime.UtcNow) {
+                cleanOlds();
+            }
+
+            if (!vals.ContainsKey(key)) vals.Add(key, new BuffEntry());
+            vals[key].setValue(val, expiredate);
+        }
+        public void setVal(string key, object val, TimeSpan expire) {
+            this.setVal(key, val, DateTime.UtcNow.Add(expire));
+        }
+        public object getVal(string key) {
+            if (!vals.ContainsKey(key)) return null;
+            return vals[key].Value;
+        }
+        public void cleanOlds() {
+            List<string> toDel = new List<string>();
+            foreach (KeyValuePair<string, BuffEntry> item in this.vals) {
+                if (item.Value.Expired) toDel.Add(item.Key);
+            }
+            foreach (string item in toDel) {
+                this.vals.Remove(item);
+            }
+            lastClean = DateTime.UtcNow;
+        }
+        public class BuffEntry {
+            object value = null;
+            DateTime lastSet = new DateTime(0);
+            DateTime expire = new DateTime(0);
+
+            public object Value {
+                set {
+                    this.value = value;
+                    lastSet = DateTime.UtcNow;
+                }
+                get {
+                    return value;
+                }
+            }
+            public DateTime LastSet {
+                get { return this.lastSet; }
+            }
+            public DateTime ExpireDate {
+                get { return this.expire; }
+            }
+            public bool Expired {
+                get {
+                    return this.expire < DateTime.UtcNow;
+                }
+            }
+            public void setValue(object val, DateTime expireDate) {
+                this.Value = val;
+                this.expire = expireDate;
+            }
+        }
+    }
+}
